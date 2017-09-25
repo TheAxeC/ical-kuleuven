90c7f39dcfac41103f9087039a44c7ca1a697e27
Axel Faes <axel.faes@gmail.com>
2017-09-25 22:05:24 +0200

    - updated schedule

--- src/ical/config.js
+++ src/ical/config.js
@@ -4,14 +4,8 @@ config.current_semester = '1';
 
 config.group_list = {
 	'axel' : {
-		'H00H2A' : 'Group A', // Uncertainty in Artificial Intelligence
-		'H0N06A' : '1McsA', // Modellering van complexe systemen
-		'H0N09A' : '1McsA', // Gedistribueerde systemen
-		'G0Q58a' : 'Group A', // Modellering en simulatie
-		'H00G6A' : 'Group B', // Machine Learning and Inductive Inference
-		'G0Q58A' : 'Group A', // Modellering en simulatie
-		'H0S00A' : '1McsA', // Software architecture
-		'H01L2A' : 'McwsBinfB' // Digitale elektronica en processoren
+		//'H00I7a' : 'Exclude', 
+		//'H02K1a' : 'Exclude',
 	}
 };
 
@@ -22,7 +16,7 @@ config.users = {
 								'H04L5a', 'H04L6a', 'H09P6a', // Vergelijkende studie van imperatieve programmeertalen
 								'H03F0a', 'H0T45a', // Technisch-wetenschappelijke software
 								'H03F9a', 'H03G0a', // Parallel Computing
-								'H00H1a', 'H02D1a', 'H08M3a', // Genetic Algorithms and Evolutionary Computing
+								//'H00H1a', 'H02D1a', 'H08M3a', // Genetic Algorithms and Evolutionary Computing
 								'H05N0a' // Capita Selecta Computer Science: Artificial Intelligence
 							],
 		'51230411-2017-1-2' : [
@@ -32,13 +26,17 @@ config.users = {
  								'H0S01a', 'H0S02a', 'H0S03a', // Comparative Programming Languages
   						],
 		'51016880-2017-1-1' : [
-								'H00I6a', 'H00I7a', 'H02A0a', 'H02K1a' // 	Fundamentals of Artificial Intelligence
+								'H00I6a', 'H00I7a', 'H02A0a', 'H02K1a', // 	Fundamentals of Artificial Intelligence
+								'H00G1A', 'H02B2A' // Cognitive Science
 							],
 		'51016880-2017-1-2' : [
-								'H02D5a', // Philosophy of Mind and Artificial Intelligence
+								//'H02D5a', // Philosophy of Mind and Artificial Intelligence
 								'H00G8a', 'H02C4a', // Artificial Neural Networks
 								'H00H3a', 'H02D3a' // Support Vector Machines: Methods and Applications
-							]
+							],
+		'51016867-2017-2-1' : [
+								'H05M9A', 'H05N1A' // Bio-informatica
+		]
 	},
 	'luuk' : {
 		'51230411-2017-2-1' : [
@@ -106,7 +104,7 @@ config.users = {
 
 		'51230411-2017-2-1' : [
 						'H04L5a', 'H04L6a', 'H09P6a', // Vergelijkende studie van imperatieve programmeertalen
-						'G0L15B'  // Capita Selecta van de software engineering
+						'G0L15A'  // Capita Selecta van de software engineering
 												],
 		'52364422-2017-2-1' : [
 						'H0S01a', 'H0S02a', 'H0S03a', // Comparative Programming Languages
@@ -117,7 +115,7 @@ config.users = {
 		'51230411-2017-2-2' : [
 						'G0K31B', // Gedistribueerde software architecturen: verdiepende studie
 						'H04G4A', 'H04G5A', 'H04G6A', //Security of Network and Computer infrastructure
-						'G0L15B', // Capita Selecta van de software engineering
+						'G0L15A', // Capita Selecta van de software engineering
 					],
 	}
 };
